75102b20e33a7ca3dc25cd3b2f52d6f539dcb479
Jimmy Phan <phanjimmy02@gmail.com>
2025-10-04 16:13:52 -0400

    Play again and added function

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -2,18 +2,32 @@ import random
 emojis = {'r': '🪨', 's': '✂️', 'p': '📃'}
 choices = ('r', 'p', 's')
 
-player_choice = input('Rock, paper, or scissors? (r/p/s): ').lower()
-if player_choice not in choices:
-    print('Invalid choice')
+def get_player_choice():
+    while True:
+        player_choice = input('Rock, paper, or scissors? (r/p/s): ').lower()
+        if player_choice in choices:
+            return player_choice
+        else:
+            print('Invalid choice')
 
-cpu_choice = random.choice(choices)
+while True:
+    player_choice = get_player_choice()
 
-print(f'Player chose {emojis[player_choice]}')
-print(f'CPU chose {emojis[cpu_choice]}')
+    cpu_choice = random.choice(choices)
 
-if player_choice == cpu_choice:
-    print('Tie!')
-elif (player_choice == 's' and cpu_choice == 'p') or (player_choice == 'p' and cpu_choice == 'r') or (player_choice == 'r' and cpu_choice == 's'):
-    print('You win!')
-else:
-    print('You lose!')
+    print(f'Player chose {emojis[player_choice]}')
+    print(f'CPU chose {emojis[cpu_choice]}')
+
+    if player_choice == cpu_choice:
+        print('Tie!')
+    elif (
+        (player_choice == 's' and cpu_choice == 'p') or 
+        (player_choice == 'p' and cpu_choice == 'r') or 
+        (player_choice == 'r' and cpu_choice == 's')):
+        print('You win!')
+    else:
+        print('You lose!')
+
+    another_round = input('Continue? (y/n): ').lower()
+    if another_round == 'n':
+        break
